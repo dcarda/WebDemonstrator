a315fbabd6a2b120cce5a9a7cb8653a07abee555
Daniel Carda <GoneBy2025@CardaTechnologies.com>
2023-11-11 11:50:12 -0700

    Interim Check-In...

---
+++ setEnv.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+clear
+
+export JAVA_HOME=/d/Tools/Java/JDK/Amazon/jdk21.0.1_12
+export ANT_HOME=/d/Tools/Java/Apache/apache-ant-1.10.11
+export MAVEN_HOME=/d/Tools/Java/Apache/apache-maven-3.9.5
+
+export PATH=$JAVA_HOME/bin:$ANT_HOME/bin:$MAVEN_HOME/bin:$PATH
+
+# Update the Path
+export PATH=$JAVA_HOME/bin:$PATH
+
+# Display Information
+echo --  BASH   ----------------------------------------------------------
+which bash
+bash -version
+echo
+echo --  JAVA   ----------------------------------------------------------
+which java
+java -version
+echo
+echo --  MAVEN  ----------------------------------------------------------
+which mvn
+mvn -version
+echo
+echo --  ANT    ----------------------------------------------------------
+which ant
+ant -version
+echo
+#echo --  GRADLE   ----------------------------------------------------------
+#which gradle
+#gradle -version
+
